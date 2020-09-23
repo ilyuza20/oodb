91b5786b6e62a338e30fbded03820732ee263a8d
ilyuza20 <71411938+ilyuza20@users.noreply.github.com>
2020-09-23 12:44:54 +0300

    Update Author.java

--- lab1/Author.java
+++ lab1/Author.java
@@ -1,30 +1,12 @@
 package com.company;
 
 public class Author {
-    private String name;
-    private String surname;
 
     public Author(String name, String surname) {
-        this.name = name;
-        this.surname = surname;
+        super(String name, String surname);
     }
 
     public Author() {
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
