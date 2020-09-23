c24922ee1889fa3c652e2f24fb29218bbdda1de8
ilyuza20 <71411938+ilyuza20@users.noreply.github.com>
2020-09-23 12:41:30 +0300

    Update Client.java

--- lab1/Client.java
+++ lab1/Client.java
@@ -1,72 +1,11 @@
 package com.company;
 
 public class Client {
-    private String name;
-    private String surname;
-    private String lastName;
-    private String address;
-    private String phoneNumber;
 
     public Client(String name, String surname, String lastName, String address, String phoneNumber, Library library) {
-        this.name = name;
-        this.surname = surname;
-        this.lastName = lastName;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-        library.addClient(this);
+        super(name, surname, lastName, address, phoneNumber);
     }
 
     public Client() {
-    }
-
-    @Override
-    public String toString() {
-        return "Client{" +
-                "name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", address='" + address + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                '}';
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
     }
 }
