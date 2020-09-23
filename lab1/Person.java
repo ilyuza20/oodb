80f1df2f06fff87f1fd187c8a65b4e203c0ceca5
ilyuza20 <71411938+ilyuza20@users.noreply.github.com>
2020-09-23 12:40:38 +0300

    Add files via upload

---
+++ lab1/Person.java
@@ -0,0 +1,71 @@
+package com.company;
+
+public class Person {
+    public String name;
+    public String surname;
+    public String lastName;
+    public String address;
+    public String phoneNumber;
+
+    public Person(String name, String surname, String lastName, String address, String phoneNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Person() {
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
